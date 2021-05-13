b538f4a28be1bcf07c3ca32237945fa0e3133963
MatuAcosta <acosta_matias8@hotmail.com>
2021-05-13 19:26:36 -0300

    Update App.js

--- empresa/src/pages/Agenda/AgendaPage.js
+++ empresa/src/pages/Agenda/AgendaPage.js
@@ -75,45 +75,4 @@ export default function AgendaPage() {
     </Container>
       
   );
-=======
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import ListGroup from 'react-bootstrap/ListGroup';
-import "./style.css";
-
-export default function AgendaPage() {
-  return (
-    <div className="container">
-      <div className ="header">
-        <h1>AGENDA</h1>
-      </div>
-      <div className = 'listaReuniones'>
-        <Card className='bg-tareas'>
-          <Card.Header>ULTIMAS ACCIONES</Card.Header>
-          <Card.Body  >
-            <Card.Text>
-              <ListGroup>
-              <ListGroup.Item action variant="info">
-                Venta Realizada
-              </ListGroup.Item>
-              <ListGroup.Item action variant="info">
-                Venta Realizada
-              </ListGroup.Item>
-              <ListGroup.Item action variant="info">
-                Venta Realizada
-              </ListGroup.Item>
-              <ListGroup.Item action variant="info">
-                Venta Realizada
-              </ListGroup.Item>
-              <ListGroup.Item action variant="info">
-                Alquiler Realizado
-              </ListGroup.Item>
-              </ListGroup>
-            </Card.Text>
-              <Button variant="success">Nueva Propiedad</Button>
-          </Card.Body>
-        </Card>
-      </div>
-    </div>
-  )
 }
