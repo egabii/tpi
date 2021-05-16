d5c35ed589e7a9d0f5385a0275fceacef8e49f49
Gabriel Muller <ernesto.gabriel90@gmail.com>
2021-05-16 14:36:31 -0300

    agenda con tarjetas

--- empresa/src/pages/Agenda/AgendaPage.js
+++ empresa/src/pages/Agenda/AgendaPage.js
@@ -1,15 +1,39 @@
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import Schedule from './Schedule/Schedule.js'
+import Card from 'react-bootstrap/Card';
+import citasModel from './citas.model';
+import './agenda.css';
 
 export default function AgendaPage() {
+  const [citas, setCitas] = useState(() => citasModel);
+
+/*   TODO: const [filtros, setFiltros] = useState([
+    {'hoy': true,  'semana': false, 'mes': false}
+  ]); */
+
   return (
-    <Container>
-      <div className="titulo"><h2 className ="letra-titulo">Tu Agenda</h2></div>
+    <Container fuild>
+      <h2>Agenda</h2>
       <Row>
-        <Col >        
-          <Schedule/>
+        <Col md={12}>
+          <section className="section-appts">
+            { citas.map(cita => <Card className="card-appts">
+              <Card.Body>
+                <Card.Title>{cita.asunto}</Card.Title>
+                <Card.Text>
+                  <p>Cliente {cita.cliente.nombre}</p>
+                  <p>Agente {cita.agente}</p>
+                  <p>Fecha: {cita.fecha}</p>
+                </Card.Text>
+              </Card.Body>
+              <Card.Footer className="text-center">
+                <Card.Link href="#">Ver Detalle</Card.Link>
+              </Card.Footer>
+            </Card>)
+            }
+          </section>
         </Col>
       </Row>
     </Container>
---
+++ empresa/src/pages/Agenda/citas.model.js
@@ -0,0 +1,88 @@
+const CitasModel = [
+  {
+    agente: 'Matias Acosta',
+    agenteId: '20340019020', // CUIT
+    citaId: 'cita.0001',
+    fecha: '20/05/2021 2:00 PM',
+    propiedadId: 'propiedad.0001',
+    cliente: {
+      nombre: 'Marcelo Casco',
+      telefono: '+5493624892201',
+      correoElectronico: 'marce.casco@email.com',
+      clienteId: 'cliente.910101'
+    },
+    asunto: 'Tasar'
+  },
+  {
+    agente: 'Matias Acosta',
+    agenteId: '20340019020', // CUIT
+    citaId: 'cita.0001',
+    fecha: '20/05/2021 2:00 PM',
+    propiedadId: 'propiedad.0001',
+    cliente: {
+      nombre: 'Marcelo Casco',
+      telefono: '+5493624892201',
+      correoElectronico: 'marce.casco@email.com',
+      clienteId: 'cliente.910101'
+    },
+    asunto: 'Tasar'
+  },
+  {
+    agente: 'Matias Acosta',
+    agenteId: '20340019020', // CUIT
+    citaId: 'cita.0001',
+    fecha: '20/05/2021 2:00 PM',
+    propiedadId: 'propiedad.0001',
+    cliente: {
+      nombre: 'Marcelo Casco',
+      telefono: '+5493624892201',
+      correoElectronico: 'marce.casco@email.com',
+      clienteId: 'cliente.910101'
+    },
+    asunto: 'Tasar'
+  },
+  {
+    agente: 'Matias Acosta',
+    agenteId: '20340019020', // CUIT
+    citaId: 'cita.0001',
+    fecha: '20/05/2021 2:00 PM',
+    propiedadId: 'propiedad.0001',
+    cliente: {
+      nombre: 'Marcelo Casco',
+      telefono: '+5493624892201',
+      correoElectronico: 'marce.casco@email.com',
+      clienteId: 'cliente.910101'
+    },
+    asunto: 'Tasar'
+  },
+  {
+    agente: 'Matias Acosta',
+    agenteId: '20340019020', // CUIT
+    citaId: 'cita.0001',
+    fecha: '20/05/2021 2:00 PM',
+    propiedadId: 'propiedad.0001',
+    cliente: {
+      nombre: 'Marcelo Casco',
+      telefono: '+5493624892201',
+      correoElectronico: 'marce.casco@email.com',
+      clienteId: 'cliente.910101'
+    },
+    asunto: 'Tasar'
+  },
+  {
+    agente: 'Matias Acosta',
+    agenteId: '20340019020', // CUIT
+    citaId: 'cita.0001',
+    fecha: '20/05/2021 2:00 PM',
+    propiedadId: 'propiedad.0001',
+    cliente: {
+      nombre: 'Marcelo Casco',
+      telefono: '+5493624892201',
+      correoElectronico: 'marce.casco@email.com',
+      clienteId: 'cliente.910101'
+    },
+    asunto: 'Tasar'
+  }
+];
+
+export default CitasModel;
