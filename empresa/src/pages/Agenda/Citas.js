67a3082e4c8b5ffad6fa39ffe53cdc31ef2fbcee
gonza zoloaga <gonzalozoloaga44@gmail.com>
2021-05-17 19:14:27 -0300

    Se agrego tabla de solicitudes

--- empresa/src/pages/Agenda/Citas.js
+++ empresa/src/pages/Agenda/Citas.js
@@ -3,7 +3,46 @@
 const Citas = () => {
     return (
         <div>
-            <h2>Carri gil de mierdaaaaaa</h2>
+            <div >
+                <h1>Solicitudes</h1>
+            </div>
+            <div className ="body">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Telefono</th>
+                        <th scope="col">Fecha</th>
+                        <th scope="col">Horario</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td><button class="btn-primary">Ver solicitud</button></td>
+                        </tr>
+                        <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                        <td>
+                            <button class="btn-primary">Ver solicitud</button>
+                        </td>
+                        </tr>
+                        <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                        <td><button class="btn-primary">Ver solicitud</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
