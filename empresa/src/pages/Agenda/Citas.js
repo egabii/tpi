91bb7a5bd3ca011d0fe9ea157454ee228784cfaa
gonza zoloaga <gonzalozoloaga44@gmail.com>
2021-05-17 20:38:16 -0300

    Pop Up

--- empresa/src/pages/Agenda/Citas.js
+++ empresa/src/pages/Agenda/Citas.js
@@ -1,50 +1,115 @@
 
+import Table from 'react-bootstrap/Table';
+import Popup from "./componentsagenda/PopupSolicitud";
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Citas = () => {
+    const solicitudes=[
+        {
+            item: 1,
+            apeyNom: 'Gonzalo Zoloaga',
+            dni: '12345678',
+            cuil: '20123456784',
+            fecnac: '21/09/2000',
+            telefono: '3777123456',
+            mail: <a href="mailto:gonzalozoloaga44@gmail.com">gonzalozoloaga44@gmail.com</a>,
+          },
+        {
+            item: 2,
+            apeyNom: 'Gonzalo Zoloaga',
+            dni: '12345678',
+            cuil: '20123456784',
+            fecnac: '21/09/2000',
+            telefono: '3777123456',
+            mail: <a href="mailto:gonzalozoloaga44@gmail.com">gonzalozoloaga44@gmail.com</a>,
+          },
+        {
+            item: 3,
+            apeyNom: 'Gonzalo Zoloaga',
+            dni: '12345678',
+            cuil: '20123456784',
+            fecnac: '21/09/2000',
+            telefono: '3777123456',
+            mail: <a href="mailto:gonzalozoloaga44@gmail.com">gonzalozoloaga44@gmail.com</a>,
+          },
+        {
+            item: 4,
+            apeyNom: 'Gonzalo Zoloaga',
+            dni: '12345678',
+            cuil: '20123456784',
+            fecnac: '21/09/2000',
+            telefono: '3777123456',
+            mail: <a href="mailto:gonzalozoloaga44@gmail.com">gonzalozoloaga44@gmail.com</a>,
+          },
+        {
+            item: 5,
+            apeyNom: 'Gonzalo Zoloaga',
+            dni: '12345678',
+            cuil: '20123456784',
+            fecnac: '21/09/2000',
+            telefono: '3777123456',
+            mail: <a href="mailto:gonzalozoloaga44@gmail.com">gonzalozoloaga44@gmail.com</a> ,
+          }
+
+    ];
+            const [buttonPopup, setbuttonPopup] = useState(false);
     return (
         <div>
-            <div >
-                <h1>Solicitudes</h1>
-            </div>
-            <div className ="body">
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Telefono</th>
-                        <th scope="col">Fecha</th>
-                        <th scope="col">Horario</th>
+            <Table striped bordered hover>
+            <thead>
+                <tr>
+                <th>Nro</th>
+                <th>Nombre y Apellido</th>
+                <th>DNI</th>
+                <th>Mail</th>
+                <th> </th>
+                </tr>
+            </thead>
+                <tbody>
+                {solicitudes.map(solicitud =>  
+                            <tr>
+                                <th>{solicitud.item}</th>
+                                <th>{solicitud.apeyNom}</th>
+                                <th>{solicitud.dni}</th>
+                                <th>{solicitud.mail}</th>
+                                <th><btn onClick = {() => setbuttonPopup(true)}class="btn btn-primary">Ver solicitud</btn></th>
+                            </tr>
+                   )}
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td><button class="btn-primary">Ver solicitud</button></td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        <td>
-                            <button class="btn-primary">Ver solicitud</button>
-                        </td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                        <td><button class="btn-primary">Ver solicitud</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+                </tbody>
+                   
+           </Table>
+           <Popup trigger={buttonPopup} setTrigger={setbuttonPopup}>
+                <DatosSolicitud></DatosSolicitud>
+            </Popup>
         </div>
     )
 }
 
 export default Citas
+
+function DatosSolicitud() {
+    return(
+    <div>
+        <div>
+            <h2>Larry the Bird</h2>
+            <hr/>
+        </div>
+        <div className="body">
+            <h5>Tipo cliente: No registrado</h5>
+            <h5>Asunto: Conocer propiedad</h5>
+            <h5>Correo electronico: larrybird@gmail.com</h5>
+            <h5>Telefon: 3777257042</h5>
+            <h5>Fecha solicitud: 25/05/2021 a las 19:38</h5>
+            <h5>Codigo Propiedad: 183</h5>
+            <h5>Comentario: que se yo</h5>
+            <button class="close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <Link to="/agenda"><btn class="btn btn-success">Asignar agente</btn></Link>
+
+        </div>
+    </div>
+    )
+}
