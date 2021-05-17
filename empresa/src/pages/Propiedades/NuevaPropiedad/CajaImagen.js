1b4c2279c910a89d2d9b89dab37564dbcb1a0969
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2021-05-17 10:36:49 -0300

    Upload Preview Image

--- empresa/src/pages/Propiedades/NuevaPropiedad/CajaImagen.js
+++ empresa/src/pages/Propiedades/NuevaPropiedad/CajaImagen.js
@@ -1,22 +1,11 @@
 import "./estiloimagen.css"
+import AppImagen from "./AppImagen"
 
 export default function Imagenes() {
     return (
     <div class="card-group">
-        
         <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-            </div>
+            <AppImagen/>
         </div>
     </div>
     );
